b77636a04981e1d1051ed9caa42e4fe317f7b224
Gianluca Brunelli <giallanon@gmail.com>
2020-02-24 13:16:33 +0100

    linux

--- src/CPUBridge/CPUBridgeServer.cpp
+++ src/CPUBridge/CPUBridgeServer.cpp
@@ -2161,7 +2161,7 @@ void Server::priv_enterState_normal()
 void Server::priv_handleState_normal()
 {
 	const u32 TIME_BETWEEN_ONE_STATUS_MSG_AND_ANOTHER_MSec = 250;
-	const u8 ALLOW_N_RETRY_BEFORE_COMERROR = 5;
+	const u8 ALLOW_N_RETRY_BEFORE_COMERROR = 10;
 	u8 nRetry = 0;
 
 	u64	nextTimeSendCheckStatusMsgWasMSec = 0;
